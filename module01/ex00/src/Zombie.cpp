c2831d483d6b052901d8fcf9174ff88ce68d3736
Andrea Bertolini <andrea.bertolini@deliveryhero.com>
2024-05-27 13:13:26 +0200

    Ex01: Add underscore to private variables

--- module01/ex00/src/Zombie.cpp
+++ module01/ex00/src/Zombie.cpp
@@ -1,13 +1,13 @@
 #include "../include/Zombie.hpp"
 
 Zombie::Zombie(std::string n) {
-    name = n;
+    _name = n;
 }
 
 Zombie::~Zombie() { 
-    std::cout << this->name << " is destroyed" << std::endl;
+    std::cout << this->_name << " is destroyed" << std::endl;
 }
 
 void    Zombie::announce(void) {
-    std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+    std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
